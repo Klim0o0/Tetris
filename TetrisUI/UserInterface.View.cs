02e96fefa31ea7363e0d990bc8b4231ff210550a
waverma <dddg.dddfgg@yandex.ru>
2021-05-23 17:23:04 +0500

    solve nested rotate

--- TetrisUI/UserInterface.View.cs
+++ TetrisUI/UserInterface.View.cs
@@ -1,42 +1,79 @@
 ﻿using SFML.Graphics;
 using SFML.System;
+using System.Collections.Generic;
+using TetrisViewModel;
 
 namespace TetrisUI
 {
-    public partial class UserInterface : Transformable, Drawable
+    public class NodeDrawble : Transformable, Drawable
     {
-        private readonly RectangleShape _fillShape = new();
-        private readonly Text _text = new();
+        private readonly List<NodeDrawble> nested = new();
+        public readonly Vector2f Size;
+        public readonly SFML.Graphics.Color FillColor;
+        private readonly Text _text;
+
+        public NodeDrawble(DrawInfo drawInfo, Vector2f parentPoint, Vector2f parentSize)
+        {
+            var ((x, y), (w, h), (r, g, b, a), rotate, text, nested) = drawInfo;
+            x += w / 2.0;
+            y += h / 2.0;
+            var pos = new Vector2f((float)x * parentSize.X / 100f + parentPoint.X, (float)y * parentSize.Y / 100f + parentPoint.Y);
+            var size = new Vector2f((float)w * parentSize.X / 100f, (float)h * parentSize.Y / 100f);
+
+            Origin = new(size.X / 2, size.Y / 2);
+            Position = pos;
+            Size = size;
+            FillColor = new(r, g, b, a);
+            Rotation = rotate;
+
+            if (!(text is null || text == ""))
+            {
+                _text = new();
+                _text.DisplayedString = text;
+                _text.FillColor = new(0, 0, 0, 255);
+                _text.Font = UIConsts.Font;
+            }
+
+            if (nested is null) return;
+            foreach (var nestedDrawInfo in nested)
+                this.nested.Add(new(nestedDrawInfo, new(), Size));
+        }
 
         public void Draw(RenderTarget target, RenderStates states)
         {
-            foreach (var ((x, y), (w, h), (r, g, b, a), rotate, text, nested) in _viewModel.GetDrawInfo())
+            states.Transform *= Transform;
+            var shapeRect = default(FloatRect);
+
+            using (var shape = new RectangleShape())
             {
-                var pos = new Vector2f((float)x * Size.X / 100f, (float)y * Size.Y / 100f);
-                var size = new Vector2f((float)w * Size.X / 100f, (float)h * Size.Y / 100f);
+                shape.Size = new(Size.X - 2, Size.Y - 2);
+                shape.FillColor = FillColor;
+                shape.Draw(target, states);
 
-                _fillShape.Position = pos;
-                _fillShape.Size = size;
-                _fillShape.FillColor = new(r, g, b, a);
-                //_fillShape.Transform.
-
-                _fillShape.Draw(target, states);
-
-                if (text is null || text == "")
-                {
-                    _text.DisplayedString = text;
-                    _text.Position = new(pos.X + size.X / 2 - (_text.GetLocalBounds().Width / 2), pos.Y + size.Y / 2 - (_text.GetLocalBounds().Height / 2));
-                    _text.Font = UIConsts.Font;
-                    //Text. = new((float)w * _size.X / 100f, (float)h * _size.Y / 100f);
-                    //Text.FillColor = new(r, g, b, a);
-                    
-                    _text.Draw(target, states);
-                }
+                shapeRect = shape.GetGlobalBounds();
             }
 
-            //scoreFrame.Draw(target, states);
-            //fieldFrame.Draw(target, states);
-            //minoViewerFrame.Draw(target, states);
+            var textRect = _text.GetGlobalBounds();
+            _text.Position = new(
+                 shapeRect.Left + (shapeRect.Width / 2) - (textRect.Width / 2),
+                 shapeRect.Top + (shapeRect.Height / 2) - textRect.Height
+                );
+            _text.Draw(target, states);
+
+            foreach (var di in nested)
+                di.Draw(target, states);
+        }
+    }
+
+    public partial class UserInterface : Transformable, Drawable
+    {
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            foreach (var drawInfo in _viewModel.GetDrawInfo())
+                using(var drawble = new NodeDrawble(drawInfo))
+                {
+                    drawble.Draw(target, states);
+                }
         }
     }
 }
